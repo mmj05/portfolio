113a267bf8cfe09910aa55de0b11f698a62fbdb9
muhaiminjobayer <mmj0993@gmail.com>
2025-07-30 01:16:56 -0500

    Update HeroSection title: replace gradient text with a greeting for a more personal touch, and streamline the presentation of the name.

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -7,12 +7,9 @@ const HeroSection = ({ scrollToSection }) => {
     <section id="hero" className="hero-section">
       <div className="hero-content">
         <h1 className="hero-title">
-          Muhaimin
-          <span className="gradient-text"> Jobayer</span>
+          <span className="hero-greeting">Hi, I'm</span>
+          Muhaimin Jobayer
         </h1>
-        {/* <h2 className="hero-subtitle">
-          Full-Stack Software Engineer
-        </h2> */}
         <p className="hero-description">
         I design, develop, and maintain scalable web applications with a focus on performance, usability, and clean architecture.
         </p>
